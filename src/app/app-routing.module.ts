7ec8565be07024c6c6645fda3b7e2735de3b4471
Clara López Benito <clara.e.lopez.benito@gmail.com>
2018-11-16 12:48:01 +0100

    Routing comenzado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { OpenComponent } from './open/open.component';
+import { AppComponent } from './app.component';
 
 //This is my case 
 const routes: Routes = [
+  {path: 'home', component: AppComponent, children: [ 
+    //{path: 'register', component: },
+    //{path: 'login', component: }
+    ],
+  {path: 'open', component: OpenComponent,children: [
+    //{path: 'juego1', component: },
+    //{path: 'juego2', component: },
+    //{path: 'juego3', component: },
+    //{path: 'juego4', component:}
+  ]},
+
     
 ];
 
