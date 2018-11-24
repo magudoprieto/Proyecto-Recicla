61b20bc7972e8fb8d26038566ddefeafe32e34cc
origin <magudoprieto@gmail.com>
2018-11-24 13:34:46 +0100

    empieza el trabajo en el formulario registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form:any;
+
+  constructor() {
+    /*this.form = new FormGroup = ({
+
+    });*/
+   }
 
   ngOnInit() {
   }
