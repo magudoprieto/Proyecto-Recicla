9733f22598e79909e6cc382d0155af8c32c7ef85
origin <magudoprieto@gmail.com>
2018-11-26 16:30:18 +0100

    Validacion de formulario registro a medias. PENDIENTE ENGANCHAR CON BBDD

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl,Validators, RequiredValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,15 +8,27 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  form:any;
+  form:FormGroup;
 
   constructor() {
-    /*this.form = new FormGroup = ({
+    this.form = new FormGroup({
+        email: new FormControl('',[Validators.required,Validators.email,Validators.maxLength(40)]),
+        
+    /*Minimo 8 caracteres Maximo 15 Al menos una letra mayúscula Al menos una letra minucula Al menos un dígito No espacios en blanco Al menos 1 caracter especial*/
 
-    });*/
+        contrasena: new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/)]),
+        nombre: new FormControl('',[Validators.required,Validators.maxLength(40),Validators.minLength(3)]),
+        municipio: new FormControl(),
+        provincia: new FormControl(),
+        postal: new FormControl('',[Validators.minLength(5),Validators.maxLength(5)])
+    });
    }
 
   ngOnInit() {
+  }
+
+  usuarioRegistrado(data){
+    console.log(data);
   }
 
 }
