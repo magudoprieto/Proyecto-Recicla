c3381d59c4cbab7a8ea7019a9696452783b4e56d
origin <magudoprieto@gmail.com>
2018-12-11 15:41:13 +0100

    Tabla puntuaciones renderizada en el proyecto angular en html

--- src/app/puntuacion/puntuacion.component.ts
+++ src/app/puntuacion/puntuacion.component.ts
@@ -8,11 +8,17 @@ import { PeticionesService } from '../peticiones.service';
 })
 export class PuntuacionComponent implements OnInit {
 
+  partidas:any[];
+  //EN EL FUTURO HACER UN ARRAY SOLO CON LAS 10 MEJORES PUNTUACIONES
+  //EN EL FUTURO MODIFICAR LA PETICION PARA QUE DEVUELVA POR ORDEN DE RANKEO
+
   constructor(private peticionesService: PeticionesService) { }
 
   ngOnInit() {
     this.peticionesService.getPartidas().then((res) => {
       console.log(res.json());
+      this.partidas = res.json();
+      console.log(this.partidas);
     })
   }
 }
